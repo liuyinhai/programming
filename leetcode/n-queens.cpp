bc6c3d6adfda789b445767bbd72f774dbb459fb8
liuyinhai <yinhai.liu@gmail.com>
2013-10-18 16:10:31 +0800

    first commit

---
+++ leetcode/n-queens.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string> > solveNQueens(int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
